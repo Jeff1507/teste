be8607c249e56f8496ecf8026da240c6dcf03e8f
Jefferson <Dibloob@outlook.com>
2023-09-03 17:46:30 -0300

    tabelas criadas

--- src/main/resources/ifpr/pgua/eic/colecaomusicas/sqls/init.sql
+++ src/main/resources/ifpr/pgua/eic/colecaomusicas/sqls/init.sql
@@ -25,3 +25,16 @@ CREATE TABLE IF NOT EXISTS musicas (
     FOREIGN KEY (generoId) REFERENCES generos(id),
     
 );
+CREATE TABLE IF NOT EXISTS playlist (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS playlist_musica (
+    playlist_musica_id int NOT NULL AUTO_INCREMENT,
+    playlist_id int NOT NULL,
+    musica_id int NOT NULL,
+    PRIMARY KEY (playlist_musica_id),
+    FOREIGN KEY (playlist_id) REFERENCES playlist(id),
+    FOREIGN KEY (musica_id) REFERENCES musicas(id)
+);
