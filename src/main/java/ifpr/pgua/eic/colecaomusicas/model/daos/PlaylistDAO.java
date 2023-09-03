61db2cd986e14cfbf0f8b668b16830a4ca5783a4
Jefferson <Dibloob@outlook.com>
2023-09-03 18:01:28 -0300

    CR feito

---
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/model/daos/PlaylistDAO.java
@@ -0,0 +1,12 @@
+package ifpr.pgua.eic.colecaomusicas.model.daos;
+
+import com.github.hugoperlin.results.Resultado;
+
+import ifpr.pgua.eic.colecaomusicas.model.entities.Playlist;
+
+public interface PlaylistDAO {
+    Resultado criar(Playlist playlist);
+
+    Resultado listar();
+
+}
