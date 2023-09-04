683115edf7c3329399f0702f3a0257f25f86449a
Jefferson <Dibloob@outlook.com>
2023-09-04 16:31:03 -0300

    sei la

--- src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/CadastroPlaylist.java
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/CadastroPlaylist.java
@@ -5,6 +5,7 @@ import ifpr.pgua.eic.colecaomusicas.model.repositories.RepositorioPlaylist;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
